48bb6f7be89b6c4ea127fb4cf478bd26bbd03969
kieranmacdonald <kieranmacdonaldschool@gmail.com>
2020-06-08 18:54:23 +0100

    added nav bar on scroll, updated css

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,16 @@
 var scrollUpBtn = document.getElementById("up-icon");
+var scrollNavBar = document.getElementById("fixed-nav");
 
 // When the user scrolls down 100px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     scrollUpBtn.style.display = "block";
+    scrollNavBar.style.display = "block";
   } else {
     scrollUpBtn.style.display = "none";
+    scrollNavBar.style.display = "none";
   }
 }
 

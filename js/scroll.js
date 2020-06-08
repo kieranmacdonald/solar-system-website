8b83030e15604fb3f0b52c6284047311d88eb6fe
kieranmacdonald <kieranmacdonaldschool@gmail.com>
2020-06-08 13:36:05 +0100

    added more planets, updated css, added scroll to top button

---
+++ js/scroll.js
@@ -0,0 +1,18 @@
+var scrollUpBtn = document.getElementById("up-icon");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollUpBtn.style.display = "block";
+  } else {
+    scrollUpBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
